f19787fc511b463f3ce274a0036843e4ee135d11
DKen-DevCat <dken.devdev@gmail.com>
2025-08-13 20:05:26 +0900

    サイドバーの追加

---
+++ src/components/common/sidebar.tsx
@@ -0,0 +1,45 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
+import { menu } from './consts';
+
+export interface Props {
+  variant?: 'sidebar' | 'floating' | 'inset';
+  collapsible?: 'offcanvas' | 'icon' | 'none';
+}
+
+export function AppSidebar({
+  variant = 'sidebar',
+  collapsible = 'offcanvas',
+}: Props) {
+  return (
+    <Sidebar variant={variant} collapsible={collapsible}>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {menu.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
